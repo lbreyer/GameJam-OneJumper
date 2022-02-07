69d54aa2998d0d5e3ddf5e1b2a0809776dc1ae8f
Luke Breyer <lukedbreyer@gmail.com>
2022-02-07 13:11:50 +0100

    Added TestScene

--- Assets/Scripts/TestPlayer.cs
+++ Assets/Scripts/TestPlayer.cs
@@ -4,15 +4,31 @@ using UnityEngine;
 
 public class TestPlayer : MonoBehaviour
 {
-    // Start is called before the first frame update
+    [Tooltip("The player movement speed.")]
+    public float speed;
+
+    private float direction = 1;
+    private Rigidbody2D rb2D;
+
     void Start()
     {
-        
+        rb2D = GetComponent<Rigidbody2D>();
     }
 
-    // Update is called once per frame
     void Update()
     {
-        
+        Vector2 movement = new Vector2(1, 0) * speed * direction;
+
+        rb2D.velocity = movement;
+
+        if (Input)
+    }
+
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        if (collision.gameObject.CompareTag("Wall"))
+        {
+            direction *= -1;
+        }
     }
 }
